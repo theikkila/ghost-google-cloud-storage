b931c2c2d6546651bdbe01e4931e8f82d28ef58f
Kay Schneider  / ikay <kay@yakisch.com>
2018-12-26 11:12:35 +0100

    added debug information, 0.0.3 version of basestorage

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 var storage     = require('@google-cloud/storage'),
     BaseStore   = require('ghost-storage-base'),
-    path        = require('path'),
     Promise     = require('bluebird'),
+    path        = require('path'),
     options     = {};
 
 class GStore extends BaseStore {
@@ -16,8 +16,7 @@ class GStore extends BaseStore {
             keyFilename: options.key
         });
         this.bucket = gcs.bucket(options.bucket);
-        this.assetDomain = options.assetDomain || `${options.bucket}.storage.googleapis.com`;
-        // only set insecure from config if assetDomain is set
+        this.assetDomain = option.assetDomain || `storage.googleapis.com/${options.bucket}/`;
         if(options.hasOwnProperty('assetDomain')){
             this.insecure = options.insecure;
         }
@@ -25,27 +24,50 @@ class GStore extends BaseStore {
         this.maxAge = options.maxAge || 2678400;
     }
 
-    save(image) {
+    /**
+     *
+     * @param image  image is the express image object
+     * @param targetDir
+     * @returns {*}
+     */
+    save(image, targetDir) {
+        console.log(image, 'image');
+        console.log(targetDir, 'targetDir');
         if (!options) return Promise.reject('google cloud storage is not configured');
-
+        console.log(image);
+        console.log(this.getTargetDir(), 'DIR');
         var targetDir = this.getTargetDir(),
-        googleStoragePath = `http${this.insecure?'':'s'}://${this.assetDomain}/`,
-        targetFilename;
+            googleStoragePath = `http${this.insecure?'':'s'}://${this.assetDomain}`;
+        var targetFilenameOut=null;
 
-        return this.getUniqueFileName(image, targetDir).then(newFile => {
-            targetFilename = newFile;
-            var opts = {
-                destination: newFile,
-                metadata: {
-                    cacheControl: `public, max-age=${this.maxAge}`
-                },
-                public: true
-            };
-            return this.bucket.upload(image.path, opts);
-        }).then(function (data) {
-            return googleStoragePath + targetFilename;
-        }).catch(function (e) {
-            return Promise.reject(e);
+        return new Promise((resolve, reject) => {
+            this.getUniqueFileName(image, targetDir).then(targetFilename => {
+                console.log(
+                    targetFilename, 'TARGETFILENAME' );
+
+                var fileNamePath=null;
+                if(targetFilename.indexOf(targetDir) === -1) {
+                    fileNamePath =targetDir + targetFilename;
+                } else {
+                    fileNamePath=targetFilename;
+                }
+                targetFilenameOut=fileNamePath;
+                var opts = {
+                    destination: fileNamePath,
+                    metadata: {
+                        cacheControl: `public, max-age=${this.maxAge}`
+                    },
+                    public: true
+                };
+                return this.bucket.upload(image.path, opts);
+            }).then(function (data) {
+                console.log(data);
+                console.log(data[0].name);
+                console.log('RESOVLE UPLOAD', targetFilenameOut);
+                return resolve( googleStoragePath  + targetFilenameOut);
+            }).catch(function (e) {
+                return reject(e);
+            });
         });
     }
 
@@ -66,25 +88,41 @@ class GStore extends BaseStore {
     }
 
     read (filename) {
-        var rs = this.bucket.file(filename).createReadStream(), contents = null;
-        return new Promise(function (resolve, reject) {
-            rs.on('error', function(err){
-                return reject(err);
+        console.log(filename, 'WHAT DA HECK');
+        //console.log(this.bucket);
+
+        if(typeof filename.path !== 'undefined') {
+            filename=filename.path;
+        }
+        const file = this.bucket.file(filename);
+
+        try {
+            var st=file.createReadStream()
+
+            var rs = this.bucket.file(filename).createReadStream();//, contents = ''
+            var contents='';
+            return new Promise(function (resolve, reject) {
+                rs.on('error', function(err){
+                    console.log('error read file');
+                    //console.log(err);
+                    return reject(err);
+                });
+                rs.on('data', function(data){
+                    contents += data;
+                });
+                rs.on('end', function(){
+                    console.log('END');
+                    //console.log(contents);
+                    return resolve(contents);
+                });
             });
-            rs.on('data', function(data){
-                if (contents) {
-                    contents = data;
-                } else {
-                    contents = Buffer.concat([contents, data]);
-                }
-            });
-            rs.on('end', function(){
-                return resolve(content);
-            });
-      });
+        } catch(e){
+            console.log('STREAM TO DEATH', e);
+        }
     }
 
     delete (filename) {
+        console.log('delete', filename);
         return this.bucket.file(filename).delete();
     }
 }
